83ca7b52ffc23d71e3499df360ac92d5752302cc
Bjorn Stromberg <bstromberg@wizcorp.jp>
2013-11-22 14:59:51 +0900

    v0.0.5: Don't bother detecting, unTome can handle it.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Tome = require('tomes').Tome;
 var log = console.log;
 console.log = function () {
 	for (var i = 0; i < arguments.length; i += 1) {
-		arguments[i] = Tome.isTome(arguments[i]) ? Tome.unTome(arguments[i]) : arguments[i];
+		arguments[i] = Tome.unTome(arguments[i]);
 	}
 	log.apply(console, arguments);
 }
