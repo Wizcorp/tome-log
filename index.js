0096d68f965fc386cd17b1552aefb326f6e533c0
Bjorn Stromberg <bstromberg@wizcorp.jp>
2013-11-28 10:39:21 +0900

    v0.0.6: wrapped our work in a try catch, if it fails for any reason you'll still get a console.log

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ var Tome = require('tomes').Tome;
 
 var log = console.log;
 console.log = function () {
-	for (var i = 0; i < arguments.length; i += 1) {
-		arguments[i] = Tome.unTome(arguments[i]);
+	try {
+		for (var i = 0; i < arguments.length; i += 1) {
+			arguments[i] = Tome.unTome(arguments[i]);
+		}
+	} catch (e) {
+		
 	}
 	log.apply(console, arguments);
 }
